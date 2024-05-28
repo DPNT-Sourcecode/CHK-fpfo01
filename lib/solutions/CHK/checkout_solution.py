5c5dd333128d67d2cc64586d306ef49a1eab8a9f
sbx_user1051 <sbx_user1051@169.254.43.237>
2024-05-28 12:18:49 +0000

    Tue May 28 12:18:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,7 +29,9 @@ def checkout(skus):
             applied = 0
             while True:
                 num_free_items = (skus_counter.get(code) // needed) - applied
-                if (num_free_items > 0 and code != free_item) or (num_free_items > 1 and code == free_item):
+                if num_free_items > 0:
+                    if code == free_item and skus_counter.get(code) <= num_free_items:
+                        break
                     skus_counter[free_item] = max(0, skus_counter[free_item] - 1)
                     applied += 1
                 else:
@@ -52,4 +54,5 @@ def checkout(skus):
             if occurance > 0:
                 sum += occurance * items.get(code)
     return sum
+
 
